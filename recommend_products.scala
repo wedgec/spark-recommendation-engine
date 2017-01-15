8868a41314dad3f0c05e32b357b0f1161ad5f86a
William Edgecomb <wedgecomb1@gmail.com>
2017-01-15 18:41:57 -0500

    Update comments in scala file

--- recommend_products.scala
+++ recommend_products.scala
@@ -22,56 +22,58 @@ else  if (id % 3 == 1) List("b" + value)
 else List("c" + value) ) })
 //groups productID,userID,score from same original review together in a list
 val reviewsConsolidated = reviews_raw_fixed.reduceByKey( (v1, v2)=>(v1 ::: v2))
-//we could get rid of k right here--would require modification going forward too
-//
+//removes prefixes and puts in form: (index, ((productId, userId), score) )
+//also we could eliminate k (index) in this command instead of the next one
 val reviewsRaw = reviewsConsolidated.map { case (k, v) =>  (k, v.sorted) }.map ({ case (k, v) => (k, ( (v(0).substring(1), v(1).substring(1) ), v(2).substring(1) ) ) } )
-// some items in the targeted items have ‘unkown’ user, so we cannot ignore them!
-//val reviews_clean = reviews.filter(_._2._1._2!=("unknown"))
-//With singles removed:
-//index, ((productID, userID), score)
-//val reviews_singles_removed = reviews.map { case (k, v) => (v._1._1, //List(v))}.reduceByKey((v1, v2) => v1 ::: v2).filter( { case (k, v) => (v.length > 1) } ).flatMap ( { //case (k,v) => v } )
+//Final review format: ((productID, userID), score)
 val reviews = reviewsRaw.map { case (k,v) => v }
-//Next two collect the product and user ids 
+
+//isolate productIds and userIDs and create map data structures convert between String and 
+//double representation of IDs 
 val productIds = reviews.map( { case ((productId, userId), score) =>productId })
 val userIds = reviews.map( { case ((productId, userId), score) =>userId })
-//Get a map <raw product/user id, unique id>. Used in training
+//get a map <raw product/user id, unique id>. Used in training
 val products = productIds.distinct.zipWithIndex.collectAsMap
 val users = userIds.distinct.zipWithIndex.collectAsMap
-//Reverse the previously created map used to produce the final output
+//reverse the previously created map used to produce the final output
 val productsReverse = products.map { case (l , r) => (r, l)}
-//create the rating used in training
+
+//create ratings RDD from processed reviews
 val ratings = reviews.map( { case ((productId, userId), score) => Rating(users(userId).toInt, products(productId).toInt, score.toDouble) })
-//create model:
+//create and train model:
 val rank = 10
 val numIterations = 10
 val model = ALS.train(ratings , rank, numIterations, 0.01)
 
 //RDD with the features in an array for all products
 val prodFeatures = model.productFeatures;
+//map for getting feature vector from Int ID
 val prodFeaturesMap = prodFeatures.collectAsMap;
 
 //function that returns  the dot product of 2 vectors given as arguments.  Our arguments are 
 //vectors of features of two products and the result is their similarity
 def dotProduct(vector1: Array[Double], vector2: Array[Double]): Double = { (0 to (vector1.size - 1)).toArray.map( Idx => vector1(Idx) * vector2(Idx)).sum }
-// Then do some operation to isolate the 100 target vectors (must preserve order)
-//schematically:
+
+//products to recommend for, as String IDs
 val targetProductsRaw = sc.textFile("/shared3/items-medium.txt")
-//val targetProducts = targetProductsRaw.map( prodID => products(prodID) )
-//do something like this for each value in targets (for loop over targets)
+//loop through all target products
 var ok = true
 for (targetRaw <- targetProductsRaw.collect() ){
 	ok = true
-	//targetID type long
+	//error handle if model does not recognize target product
 	if (! products.contains(targetRaw)) {
 		println(" <product " + targetRaw + " not found >")
 		ok = false
 	}
 	if (ok) {
+		//targetID of type long
 		val targetID = products(targetRaw)
+		//gets all (similarity, productID) pairs, where similarity is measured by 
+		//dot product of target product feature vector with other product feature vector
 		val similarities = prodFeatures.filter({ case (prodID, featureVec) => (prodID != targetID.toInt) } ).map( { case (prodID, featureVec) => ( dotProduct(prodFeaturesMap(targetID.toInt), featureVec), prodID.toInt ) } )
-		//more efficient, makes similarity the first value in tuple and sorts by it using reverse of 
-		//default sort
+		//gets product IDs with 10 greatest dot products--makes use of default tuple ordering, which orders by key
 		val bestTen = similarities.takeOrdered(10) (Ordering[(Double, Int)].reverse) .map({ case (similarity, prodID) => prodID } )
+		//loop writes IDs of top ten recommended products  to file
 		var i=0
 		print(targetRaw + " ")
 		for (productID <- bestTen) {
